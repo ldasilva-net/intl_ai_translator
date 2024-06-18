63b0547bd7ee84b2ae32f3ddd72c8e83e267e897
Liber Da Silva <dev@ldasilva.net>
2024-06-18 17:18:48 -0300

    Initial project

---
+++ intl_ai_translator/lib/intl_ai_translator.dart
@@ -0,0 +1,10 @@
+/// intl_ai_translator, A CLI tool that uses AI to automatically translate text copies for Dart applications utilizing the intl library
+///
+/// ```sh
+/// # activate intl_ai_translator
+/// dart pub global activate intl_ai_translator
+///
+/// # see usage
+/// intl_ai_translator --help
+/// ```
+library;
